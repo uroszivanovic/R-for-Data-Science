590ef5ed014237a26417f0c308105fc8850f2d96
Uros Zivanovic <uroszivanovic7@gmail.com>
2020-04-20 13:50:33 +0200

    editing code

--- data-visualization.R
+++ data-visualization.R
@@ -4,6 +4,3 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
   geom_point(mapping = aes(color = class)) + 
   geom_smooth()
 
-ggplot(data = mpg, mapping = aes(x = class, y = hwy)) + 
-  geom_boxplot() +
-  coord_flip()
