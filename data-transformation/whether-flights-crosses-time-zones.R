498daab994bfc0a8bf2f1293bb0d9b0861361e92
Uros Zivanovic <uroszivanovic7@gmail.com>
2020-04-21 23:57:43 +0200

    script edit

---
+++ data-transformation/whether-flights-crosses-time-zones.R
@@ -0,0 +1,21 @@
+library("nycflights13")
+library("tidyverse")
+
+time2mins <- function(x) {
+  (x %/% 100 * 60 + x %% 100) %% 1440
+  }
+
+
+flights_airtime <-
+  mutate(flights,
+         dep_time = (dep_time %/% 100 * 60 + dep_time %% 100) %% 1440,
+         arr_time = (arr_time %/% 100 * 60 + arr_time %% 100) %% 1440,
+         air_time_diff = air_time - arr_time + dep_time
+         )
+
+nrow(filter(flights_airtime, air_time_diff != 0))
+
+ggplot(flights_airtime, aes(x = air_time_diff)) +
+  geom_histogram(binwidth = 1)
+
+ggsave("data-transformation/figs/arr-dep-diff-hist.png")
