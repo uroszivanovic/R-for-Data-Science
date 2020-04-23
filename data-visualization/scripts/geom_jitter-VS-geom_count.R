e447b880173bcf5dd69005bcc054ee44518008ce
Uros Zivanovic <uroszivanovic7@gmail.com>
2020-04-24 01:55:18 +0200

    deleting files

--- data-visualization/scripts/geom_jitter-VS-geom_count.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-
-ggplot(data = mpg, mapping = aes(x = cty, y = hwy, color = class)) +
-  geom_jitter()
-
-ggplot(data = mpg, mapping = aes(x = cty, y = hwy, color = class)) +
-  geom_jitter()
