f8c4a1e8d31338fac6395205ed10cebfce92381a
Uros Zivanovic <uroszivanovic7@gmail.com>
2020-05-05 15:06:51 +0200

    changes to the code

--- exploratory-data-analysis/combination-of-techniques.R
+++ exploratory-data-analysis/combination-of-techniques.R
@@ -7,14 +7,8 @@ ggplot(diamonds, aes(x = carat, y = price)) +
   facet_wrap(~cut, ncol = 1) +
   scale_fill_viridis()
 
-ggsave("exploratory-data-analysis/figs/diamond_viridis.png")
-
 ggplot(diamonds, aes(x = cut_number(carat, 5), y = price, colour = cut)) +
   geom_boxplot() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
 
-ggsave("exploratory-data-analysis/figs/cut_price_boxplot.png")
-
 ggplot(diamonds, aes(colour = cut_number(carat, 5), y = price, x = cut)) +
   geom_boxplot() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
-
-ggsave("exploratory-data-analysis/figs/cut_price_boxplot2.png")
--- exploratory-data-analysis/diamonds-eda.R
+++ exploratory-data-analysis/diamonds-eda.R
@@ -1,19 +1,13 @@
 library(tidyverse)
 
 ggplot(data = diamonds) +
-  geom_histogram(mapping = aes(x = carat), binwidth = 0.5)
-
-ggsave("exploratory-data-analysis/figs/diamond_hist.png")
+  geom_histogram(mapping = aes(x = carat), binwidth = 0.5, colour="black")
 
 smaller <- diamonds %>% 
   filter(carat < 3)
 
 ggplot(data = smaller, mapping = aes(x = carat)) +
-  geom_histogram(binwidth = 0.1)
-
-ggsave("exploratory-data-analysis/figs/diamond_hist_smaller.png")
+  geom_histogram(binwidth = 0.1, colour="black")
 
 ggplot(data = smaller, mapping = aes(x = carat, y = price)) + 
   geom_boxplot(mapping = aes(group = cut_number(carat, 20)))
-
-ggsave("exploratory-data-analysis/figs/diamond_boxplot.png")
